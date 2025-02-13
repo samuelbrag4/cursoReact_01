7e83ccc96ce8e88b270a68079b57c5686dea2e6b
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2025-02-12 22:09:16 -0300

    replace default content in App component with Message component

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,31 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import Message from './Message'
 
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Minha Primeira Aplicação</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Contador {count}
-        </button>
-      </div>
-      <p className="read-the-docs">
-        Clique nas logos para saber mais
-      </p>
-    </>
+    <div>
+      <Message />
+    </div>
   )
 }
 
